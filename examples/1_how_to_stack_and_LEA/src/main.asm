04911b8b9dd812e8517b396efaf6313f750ba855
Teriks <Teriks@users.noreply.github.com>
2016-02-14 14:02:04 -0600

    Make comments more readable

--- examples/1_how_to_stack_and_LEA/src/main.asm
+++ examples/1_how_to_stack_and_LEA/src/main.asm
@@ -95,16 +95,16 @@ print_messages_example:
 	
 	;64 bit windows requires the stack pointer be aligned to 16 bytes.
 	;it also requires that the stack contain at least 32 bytes of space in all function calls that 
-	;call other functions.  The alignment here is for portability
+	;call other functions.  The alignment here is for portability.
 	
 	sub rsp, (56+8)
 	
 	;================================
-	;the keyword QWORD stands for QUAD WORD, a WORD is 16 bits (or two bytes)
+	;the keyword QWORD stands for QUAD WORD, a WORD is 16 bits (or two bytes).
 	;so a QWORD has 64 bits, which is big enough to hold the 64bit pointer
 	;to a message string (or any other memory)
 
-	;we need to specify the size so mov knows how many bytes of data its going to move into a memory location
+	;we need to specify the size so mov knows how many bytes of data its going to move into a memory location.
 	;some possible sizes are :
 	
 	;BYTE -> 8 bits 
@@ -114,24 +114,24 @@ print_messages_example:
 	;QWORD -> 64 bits
 
 	;we want to count by 8's in our offset, because we are moving 64 bit values which are 8 bytes long
-	;and they should not overlap at all
+	;and they should not overlap at all.
 	
 	
-	;we need to use lea, or just mov, to put the 64 bit address into a register
-	;because you cannot move a 64 bit immediate label into memory directly without it being
-	;turnicated to 32 bits, which will possibly cause the linker to complain, or worse cause your program to crash
-	;and be a hard to find bug
+	;we need to use lea or mov to put a 64 bit label address into a register before moving the address into memory.
+	;this is because you cannot move the address of a 64 bit immediate label into memory directly without it being
+	;turnicated to 32 bits.  Having the address turnicated will possibly cause the linker to complain 
+	;or worse, cause your program to crash unexpectedly; the latter being a difficult to find bug.
 	
-	;we can however, move a 64 bit immediate label (A pointer basically) into a register
-	;then move the 64 bit register into memory
-	
+
+	;move the label address into a register first with lea, then move the register
+	;contents into a memory location on the stack.
 	lea rax, [rel message_1]
 	mov QWORD [rsp],	 rax
 	
 	
-	;mov accomplishes the same thing here
-	;except we have no option of doing arithmetic on the right hand operand like lea can
-	
+	;mov accomplishes the same thing here.
+	;except we have no option of doing arithmetic on the right hand operand like lea can,
+	;and we need to specify the label address as being a QWORD.
 	mov rax, QWORD message_2
 	mov QWORD [rsp+8],	 rax
 	
@@ -184,9 +184,9 @@ print_messages_example:
 	call print_string
 	call print_nl
 	
-	;restore the previous stack frame
+	;restore the previous stack frame.
 	;we just add to rsp and equal amount to what we subtracted when we entered the function
-	;then restore the stack base pointer rbp by popping it
+	;then restore the stack base pointer rbp by popping it.
 	add rsp, (56+8)
 	pop rbp
 	ret
@@ -201,18 +201,18 @@ print_messages_example_v2:
 
 
 
-	;subtract RSP by (56+8) to make room for 7x	 8-byte pointer values on the stack 7*8=56
-	;the +8 is to align it, because 56 is not aligned to 16 bytes 
+	;subtract RSP by (56+8) to make room for 7x 8-byte pointer values on the stack 7*8=56.
+	;the +8 is to align it, because 56 is not aligned to 16 bytes.
 	;(we need a value that divides by 16 evenly)
 	sub rsp, (56+8)
 	
 
 	;================================
 	; you can also use RBP with a negative offset to address stack values, we use subtract because we are storing values
-	; in the direction in memory that moves towards RSP, (RSP resides at a lower address than RBP)
+	; in the direction in memory that moves towards RSP, (RSP resides at a lower address than RBP).
 	; we need to start at an offset equal in size to the data we are moving to memory
 	;
-	; otherwise, if we use [RBP] as the first address this will happen:
+	; otherwise, if we use [RBP] as the first address, this will happen:
 	;
 	; -- (etc..)	
 	; -- (contains another byte of pointer)
@@ -222,7 +222,7 @@ print_messages_example_v2:
 	; -- (rsp) low pointer address
 	;
 	;
-	; we would write into the calling functions stack frame and make it mad at us
+	; we would write into the calling functions stack frame and make it mad at us,
 	; probably crashing the program
 	
 	; x86_64 is a little-endian architecture, it lays out the bytes of integers from low address to high address
@@ -253,8 +253,8 @@ print_messages_example_v2:
 
 	;================================
 
-	; its safe to address [RSP] directly
-	; because all register values in x86_x64 are in little-endian format
+	; it's safe to address [RSP] directly
+	; because all register values in x86_64 are in little-endian format.
 	; (they are laid out from low byte to high byte in memory and in registers) 
 	;
 	; right now the stack is something like this (thinking in terms of individual bytes in memory):
@@ -268,10 +268,11 @@ print_messages_example_v2:
 	; -- (rsp) low address	(has the first byte of pointer to message_7)
 	; 
 	;
-	; also, when we address RSP and move it into RDI nasm knows we need to pull a quad word from memory (64 bits)
-	; because RDI is a 64bit register
+	; also, when we address RSP and move it into RDI, nasm knows we need to pull a quad word from memory (64 bits);
+	; that is because RDI is a 64bit register.
 	; 
-	; if your moving a byte of memory into a byte register like AL
+	; if your moving a byte of memory into a byte register like AL, it works the same.
+	;
 	; you could just do:
 	; 
 	; mov AL, [rsp]
@@ -280,14 +281,14 @@ print_messages_example_v2:
 	;
 
 	
-	;if we start at RSP now we will be printing in reverse order
-	;we aligned our stack to 16 bytes using +8 though, so we need to start at [RSP+8],
-	;which is equal to [RBP-56] at this point
+	;if we start at RSP now we will be printing in reverse order.
+	;we aligned our stack to 16 bytes using +8 though so we need to start at [RSP+8],
+	;which is equal to [RBP-56] at this point.
 
 	mov rdi, [rsp+8]
 	call print_string
 
-	;print RSP+8 as an unsigned 64 bit integer, so we see the address of the string, after the string that's being printed
+	;print RSP+8 as an unsigned 64 bit integer, so we see the address of the string that was just printed
 	lea rdi, [(rsp+8)]
 	call print_uint
 	call print_nl
@@ -302,8 +303,8 @@ print_messages_example_v2:
 	call print_string
 
 	; lea stands for LOAD EFFECTIVE ADDRESS, and it means:
-	; 1. take the effective address calculation on the RIGHT and compute the actual pointer value from it
-	; 2. put the calculated pointer value in the register on the LEFT 
+	; 1. take the effective address calculation on the RIGHT and compute the actual pointer value from it.
+	; 2. put the calculated pointer value in the register on the LEFT.
 
 	lea rdi, [(rsp+8) +8]
 	
@@ -371,15 +372,15 @@ print_messages_example_v2:
 
 
 fun_with_lea:
-	; I am using LEA here to show you some of the fancy expressions that can be used to address memory 
+	; I am using LEA here to show you some of the fancy expressions that can be used to address memory.
 	; there are some constraints to memory addressing, but nasm can occasionally factor your math down
-	; to meet these constraints
+	; to meet these constraints.
 	;
 	; if your affective address for memory operations do not meet processor constraints
-	; you will get an 'invalid effective address' error from nasm
-	; the constraints are a bit too long to describe here.
+	; you will get an 'invalid effective address' error from nasm.
 	;
-	; but you can read about effective addresses in nasm here:
+	; the constraints are a bit too long to describe here,
+	; but you can read about effective addresses in nasm at this link:
 	;
 	; http://www.nasm.us/doc/nasmdoc3.html#section-3.3
 	;
@@ -389,7 +390,7 @@ fun_with_lea:
 	sub rsp, 32
 	
 	;calculate a bogus address and print the result.
-	;lea can be used for arithmetic on values that are not actually pointers
+	;lea can be used for arithmetic on values that are not actually pointers,
 	;but that's sort of hacky and has limited usage due to constraints on effective addressing calculations
 	
 	mov rdi, 5
@@ -425,8 +426,8 @@ main:
 
 	;create a new stack frame
 	
-	;we first save the 'stack base pointer' which is the high address of the stack
-	;the stack is like this:
+	;we first save the 'stack base pointer' which is the high address of the stack.
+	;the stack is currently like this:
 	;
 	; -- (rbp) high pointer address
 	; --   ..probably local variable/parameter in stack memory from the function that called this one..
@@ -450,11 +451,11 @@ main:
 	
 	
 	;On windows, we need a minimum of 32 bytes of stack space before calling
-	;any functions, RSP also always needs to be aligned to 16 bytes
+	;any functions, RSP also always needs to be aligned to 16 bytes.
 	;32/16 = 2, 2 is a whole number which means 32 is indeed aligned to 16 bytes
 	
-	;this is for compatibility, Linux and Mac can run programs with or without the stack pointer being aligned
-	;but on Windows if the stack is not aligned the program will crash
+	;this is for compatibility, Linux and Mac can run programs with or without the stack pointer being aligned,
+	;but on Windows if the stack is not aligned the program will crash.
 	sub rsp, 32 
 	
 	
@@ -501,14 +502,14 @@ main:
 	
 
 
-	;restore the stack to its previous state
-	;first an equal amount to RSP as what we subtracted when we entered this function
-	;this includes the sum of all additional subtractions made after the first subtraction (if there are any)
-	;then restore the base pointer (RBP) by popping it back into itself
+	;restore the stack to its previous state.
+	;first add an equal amount to RSP as what we subtracted when we entered this function.
+	;this includes the sum of all additional subtractions made after the first subtraction (if there are any).
+	;then restore the base pointer (RBP) by popping it back into itself.
 	add rsp, 32
 	
-	;once we do the subtraction, the value of RBP we pushed at the beginning of the function
-	;should be the only thing left on the stack
+	;once we do the addition, the value of RBP we pushed at the beginning of the function
+	;should be the only thing left on the stack.
 	pop rbp
 
 	ret
